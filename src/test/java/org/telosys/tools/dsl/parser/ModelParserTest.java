d4809237adb62910d6fae291730c1571d9ecb976
Ludovic Chaboud <l.chaboud@gmail.com>
2015-03-25 14:55:22 +0100

    embedded

--- src/test/java/org/telosys/tools/dsl/parser/ModelParserTest.java
+++ src/test/java/org/telosys/tools/dsl/parser/ModelParserTest.java
@@ -58,13 +58,39 @@ public class ModelParserTest {
 		employee.addField(new DomainEntityField("birthDate", DomainNeutralTypes.getType("date")));
 		DomainEntityField countryField = new DomainEntityField("country", country);
 		countryField.addAnnotation(new DomainEntityFieldAnnotation("Max", "3"));
-		countryField.addAnnotation(new DomainEntityFieldAnnotation("Embedded"));
 		employee.addField(countryField);
 		modelToCompare.addEntity(employee);
 		assertEquals(modelToCompare,model);
 	}
-	
-	@Test
+
+    @Test
+    public void testParseModelWithTwoEntitiesAndEmbedded() throws Exception {
+        File folder = new File("src/test/resources/model_test/valid/model_withTwoEntitiesAndEmbedded");
+        DomainModelParser parser = new DomainModelParser();
+        DomainModel model = parser.parse(folder);
+        DomainModel modelToCompare = new DomainModel("ModelWithTwoEntitiesAndEmbedded");
+        DomainEntity country = new DomainEntity("Country");
+        DomainEntityField idCountry = new DomainEntityField("id", DomainNeutralTypes.getType("integer"));
+        idCountry.addAnnotation(new DomainEntityFieldAnnotation("Id"));
+        country.addField(idCountry);
+        country.addField(new DomainEntityField("name", DomainNeutralTypes.getType("string")));
+        modelToCompare.addEntity(country);
+
+        DomainEntity employee = new DomainEntity("Employee");
+        DomainEntityField id = new DomainEntityField("id", DomainNeutralTypes.getType("integer"));
+        id.addAnnotation(new DomainEntityFieldAnnotation("Id"));
+        employee.addField(id);
+        employee.addField(new DomainEntityField("firstName", DomainNeutralTypes.getType("string")));
+        employee.addField(new DomainEntityField("birthDate", DomainNeutralTypes.getType("date")));
+        DomainEntityField countryField = new DomainEntityField("country", country);
+        countryField.addAnnotation(new DomainEntityFieldAnnotation("Max", "3"));
+        countryField.addAnnotation(new DomainEntityFieldAnnotation("Embedded"));
+        employee.addField(countryField);
+        modelToCompare.addEntity(employee);
+        assertEquals(modelToCompare,model);
+    }
+
+    @Test
 	public void testParseModelWithAnEnum() throws Exception {
 		File folder = new File("src/test/resources/model_test/valid/model_withAnEnum");
 		DomainModelParser parser = new DomainModelParser();
