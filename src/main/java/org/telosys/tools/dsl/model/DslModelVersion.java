7e7ce2037e5acea8f4dd2c84b246ac98f0147438
l-gu <laurent@telosys.org>
2023-08-25 16:52:54 +0200

    Version 4.1.0

--- src/main/java/org/telosys/tools/dsl/model/DslModelVersion.java
+++ src/main/java/org/telosys/tools/dsl/model/DslModelVersion.java
@@ -20,7 +20,7 @@ package org.telosys.tools.dsl.model;
  */
 public class DslModelVersion {
 
-    public static final String VERSION = "4.0.0";
+    public static final String VERSION = "4.1.0";
 
     private DslModelVersion(){}
 }
